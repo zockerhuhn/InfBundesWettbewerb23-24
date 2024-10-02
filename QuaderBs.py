101934f010c283a5540e94bc44aa3ef8197bd3f7
zockerhuhn <fbxfbxfbx5@gmail.com>
2024-10-02 15:25:15 +0200

    works

--- QuaderBs.py
+++ QuaderBs.py
@@ -1,8 +1,7 @@
-import numpy as np
+from copy import deepcopy
 
 quaderIsOpen = []
 quaderTimes = [5,8,12]
-#runs = np.array([[0, 1], [2, 3]], int)
 runs = [[],[]]
 for i in quaderTimes:
     quaderIsOpen.append(False)
@@ -17,20 +16,26 @@ while not found:
             else:
                 quaderIsOpen[x]=True
     if quaderIsOpen[0]:
-        #runs = np.append(runs, loop)
         runs.append([loop])
-    runsTemp = runs
+    runsTemp = deepcopy(runs)
     for i in runs:
         reached = len(i)-1
         if not quaderIsOpen[reached]:
             runsTemp.remove(i)
             continue
         if quaderIsOpen[reached+1]:
-            runsTemp[runs.index(i)].append(loop)
+            t=0
+            for e in i:
+                t+=e
+            t = (loop)-t
+            runsTemp[runs.index(i)].append(t)
             runsTemp.append(i)
             if reached+1==len(quaderTimes)-1:
-                print(i)
+                for z in runsTemp[runs.index(i)]:
+                    print(f"warte {z} Minuten")
                 found=True
+                break
+    runs = deepcopy(runsTemp)
     print(runs)
     loop+=1
     if loop>=50:
