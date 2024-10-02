1f459969a9faedc2d14f90a53e0b07f7d5c311a1
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-10-02 14:34:56 +0200

    Update QuaderBs.py

--- QuaderBs.py
+++ QuaderBs.py
@@ -3,12 +3,12 @@ import numpy as np
 quaderIsOpen = []
 quaderTimes = [5,8,12]
 #runs = np.array([[0, 1], [2, 3]], int)
-runs = [[0, 0],[0, 0]]
+runs = [[],[]]
 for i in quaderTimes:
     quaderIsOpen.append(False)
 
 found = False
-loop = 0
+loop = 1
 while not found:
     for x in range(len(quaderIsOpen)):
         if loop%quaderTimes[x]==0 and loop>0:
@@ -19,12 +19,19 @@ while not found:
     if quaderIsOpen[0]:
         #runs = np.append(runs, loop)
         runs.append([loop])
-        print(runs[0])
-        print(runs[1])
-        print(runs)
+    print(runs)
+    runsTemp = runs
     for i in runs:
-        reached = i[i.len()-1]
-        if not quaderIsOpen[reached]
+        reached = i.len()-1
+        if not quaderIsOpen[reached]:
+            runsTemp.remove(i)
+            continue
+        if quaderIsOpen[reached+1]:
+            runsTemp[runs.index(i)].append(loop)
+            runsTemp.append(i)
+            if reached+1==len(quaderTimes)-1:
+                print(i)
+                found=True
     loop+=1
     if loop>=50:
         found=True
