68ec6f70f80fbcf760b64568ab3881f53224a5dc
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-10-02 14:12:39 +0200

    Update QuaderBs.py

--- QuaderBs.py
+++ QuaderBs.py
@@ -2,7 +2,8 @@ import numpy as np
 
 quaderIsOpen = []
 quaderTimes = [5,8,12]
-runs = np.array([[0, 1], [2, 3]], int)
+#runs = np.array([[0, 1], [2, 3]], int)
+runs = [][]
 for i in quaderTimes:
     quaderIsOpen.append(False)
 
@@ -10,7 +11,8 @@ found = False
 loop = 0
 while not found:
     if quaderIsOpen[0]:
-        runs = np.append(runs, loop)
+        #runs = np.append(runs, loop)
+        runs.append(loop)
         print(runs[0])
         print(runs[1])
         print(runs)
