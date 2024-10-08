52cf53005174c5367b48e32a201b0285974ade21
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-10-08 13:10:28 +0200

    Aktualisieren von Grabmal.py

--- Grabmal.py
+++ Grabmal.py
@@ -2,14 +2,14 @@ from copy import deepcopy
 
 quaderIsOpen = []
 quaderTimes = []
-reachedTimes = []
+reachedTimes = [[],[]]
 with open("grabmal3.txt", 'r') as inputs:
     inputs = inputs.read().split("\n")
     for value in inputs:
         quaderTimes.append(int(value))
-for i in quaderTimes:
+for e in range(len(quaderTimes)):
     quaderIsOpen.append(False)
-    reachedTimes.append(0)
+    reachedTimes.append(e)
 
 found = False
 loop = 1
@@ -24,5 +24,22 @@ while not found:
         if i = 0:
             continue
         if quaderIsOpen[i] and (quaderIsOpen[i+1] or quaderIsOpen[i-1]):
-            
+            isContained = False
+            for y in reachedTimes[i+1]:
+                 try:
+                     reachedTimes[i].index(y)
+                     isContained = True
+                     break
+                 except Exeption():
+                     pass
+            if not isContained:
+                 for y in reachedTimes[i-1]:
+                     try:
+                         reachedTimes[i].index(y)
+                         isContained = True
+                         break
+                     except Exeption():
+                         pass
+                if not isContained:
+                    reachedTimes
     
